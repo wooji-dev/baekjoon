c9e0954e934628e8fe613b59bcce3b48392a01dd
Jungin Woo <wooji.dev@gmail.com>
2026-03-18 18:11:52 +0900

    [Silver I] Title: 스타트와 링크, Time: 4304 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,33 @@
+import sys
+from itertools import combinations
+
+# 입력 받기
+N = int(sys.stdin.readline())
+# 인덱스를 1부터 맞추기보다 0부터 사용하는 것이 파이썬에서 더 간결합니다.
+adj = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+members = list(range(N))
+min_diff = float('inf')
+
+# 1. 전체 인원의 절반을 뽑아 한 팀(start_team)을 구성
+for start_team in combinations(members, N // 2):
+    # 2. 나머지 인원으로 상대 팀(link_team) 구성
+    link_team = list(set(members) - set(start_team))
+
+    start_score = 0
+    link_score = 0
+
+    # 3. 각 팀 내에서 2명씩 조합하여 능력치 합산
+    for i, j in combinations(start_team, 2):
+        start_score += adj[i][j] + adj[j][i]
+
+    for i, j in combinations(link_team, 2):
+        link_score += adj[i][j] + adj[j][i]
+
+    # 4. 두 팀의 점수 차이 최솟값 갱신
+    min_diff = min(min_diff, abs(start_score - link_score))
+
+    if min_diff == 0:
+        break
+
+print(min_diff)
