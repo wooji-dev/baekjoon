8b9f6cd2b1cb60bf75c97d645a1e4dc759ef276e
Jungin Woo <wooji.dev@gmail.com>
2026-03-22 14:29:55 +0900

    [Silver I] Title: 신입 사원, Time: 5220 ms, Memory: 53180 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,31 @@
+import sys
+import itertools
+
+test = int(sys.stdin.readline())
+
+def hire(people):
+    p = []
+
+    for i in range(people):
+        man = list(map(int, sys.stdin.readline().split()))
+        if(man[0]==people and man[1]==people):
+            continue
+        else:
+            p.append(man)
+
+    p.sort()
+    flag = 1
+
+    while flag < len(p):
+        if p[flag-1][1] < p[flag][1]:
+            p.pop(flag)
+            flag = 1
+        else:
+            flag += 1
+
+    return len(p)
+
+for _ in range(test):
+    people = int(sys.stdin.readline())
+    print(hire(people))
+
