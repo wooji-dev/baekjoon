e05b3bd5435dd1f0348fa1d8723ad2169b1d5f18
Jungin Woo <wooji.dev@gmail.com>
2026-07-06 20:01:26 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,3 @@
+-- 코드를 입력하세요
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH from MEMBER_PROFILE
+where DATE_OF_BIRTH like '%03%' and gender = 'W' and tlno is not null
